681950c2db234717a0d19335e77f37605e936327
Elizabeth Tucker <bits.tucker@gmail.com>
2023-10-02 19:52:32 -0400

    javascript and html revisions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,61 @@
 var timeEl = document.querySelector("#timer");
+var questionEl = document.querySelector(".ask-ques")
 var secondsLeft = 120;
 
-var questionList = ['What special characters denote a string in Javascript?', 'What special characters denote an array in JavaScript?', 'What special characters denote that two values are strictly equal in JavaScript?', 'Which of the following is not a type of loop in JavaScript?', 'What property would you use to remove the last element in an array in JavaScript?']
+var askQuestion = 0
 
-var answerCorrect = [' " "', ' []', ' ===', ' integer', ' .pop()']
+var questionList = [
+        {
+                question: 'What special characters denote an array in JavaScript?',
+                correctAns: '[]',
+                answers: ['{}', '&&', '[]', '()']
+
+        }, 
+        {
+                question: 'What special characters denote a string in JavaScript?',
+                correctAns: '""',
+                answers: ['{}', '&&', '""', '()']
+        }, 
+        {
+                question: 'What special characters denote that two values are strictly equal in JavaScript?',
+                correctAns: '===',
+                answers: ['{}', '&&', '===', '()']
+        }
+]
+
+// console.log(questionList[askQuestion].question)
+// askQuestion++
+// console.log(questionList[askQuestion].question)
+
+// , 'Which of the following is not a type of loop in JavaScript?', 'What property would you use to remove the last element in an array in JavaScript?']
+
+// var answerCorrect = [' " "', ' []', ' ===', ' integer', ' .pop()']
 
 
 
 //init timer on question page load
 function setTime() {
-        var timerInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft + ' seconds remaining';  
-        if (secondsLeft === 0) {
-        var noTime = window.confirm("Your time has run out! Click OK to view High Scores, click Cancel to start again.")
-        clearInterval(timerInterval)
-        // else if (noTime) {
-        // go to high scores
-        // } else {
-        // return
-        }
-}, 1000);
+        var timerInterval = setInterval(function () {
+                secondsLeft--;
+                timeEl.textContent = secondsLeft + ' seconds remaining';
+                if (secondsLeft === 0) {
+                        var noTime = window.confirm("Your time has run out! Click OK to view High Scores, click Cancel to start again.")
+                        clearInterval(timerInterval)
+                        // else if (noTime) {
+                        // go to high scores
+                        // } else {
+                        // return
+                }
+        }, 1000);
 }
 setTime()
 
-// function askQuestions() {
-for(let i = 0; i < questionList.length; i++) { document.getElementById("question").innerHTML = questionList[i];
-} if (answerCorrect) {
-        var nextQuestion = questionList(i+1)
-} else {
-        secondsLeft -10;
+function takeQuiz() {
+        questionEl.textContent=questionList[askQuestion].question
 }
+
+takeQuiz()
+
 
 
 //hide questions 2-5 when question page loads
